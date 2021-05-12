d6d69a3017c0995246b914b45800b8ff124dc27c
Noler1 <lobanovanton11@gmail.com>
2021-05-12 22:37:32 +0300

    First

---
+++ Variable.java
@@ -0,0 +1,19 @@
+import java.lang.String;
+
+public class Variable {
+
+    public static void main(String [] args) {
+
+    	byte ozu = 4;
+    	short harddisk = 2048;
+    	int windows = 10;
+    	long graphics = 940;
+    	float corei7 = 2.4f;
+    	double type6 = 600.6;
+    	char type7 = 'A';
+    	boolean type8 = true;
+
+    	System.out.println(type7);
+
+	}
+} 
